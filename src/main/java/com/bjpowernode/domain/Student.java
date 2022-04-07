a11297762b65e04c73fc8afadc133d4b7d1d95d0
zs <zs@bjpowernode.com>
2022-04-07 18:39:34 +0800

    添加Student类中的方法

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -2,6 +2,11 @@ package com.bjpowernode.domain;
 
 public class Student {
     public static void main(String[] args) {
+        doget();
         System.out.println("hello world");
+    }
+
+    private static void doget() {
+        System.out.println("This is get mether");
     }
 }
